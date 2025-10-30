63d3637764a237bd905767644a6960baab8bdcd5
justpeterjp1 <oluwagbotemiojopeter10@gmail.com>
2025-10-30 04:34:09 +0100

    api corrections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
     setStatus("Sending...");
 
     try {
-      const response = await fetch("https://portfolio-y1ms.onrender.com", {
+      const response = await fetch("https://portfolio-y1ms.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
